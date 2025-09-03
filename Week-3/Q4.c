de09b8cb6e4ddcca56cde7bc2f907de666fa572b
SandeepKumarKuanar <sandeepkumarkuanar3@gmail.com>
2025-09-03 16:01:43 +0530

    Adding the missing files

--- Week-3/Q4.c
+++ Week-3/Q4.c
@@ -15,8 +15,45 @@ int main(){
 	int userInput;
 	printf("Give the input\n");
 	scanf("%d", &userInput);
+	float total;
+	if (userInput < 25) {
+		total += 4.89 * userInput;
+	} else if (userInput > 25 && userInput < 25 + 35){
+		total += 4.89 * 25;
+		total += 5.40 * (userInput - 25);
+	} else if (userInput > 60 && userInput < 100){
+		total += 4.89 * 25;
+		total += 5.40 * 35;
+		total += 6.41 * (userInput - 60);
+	} else if (userInput > 100 && userInput < 150){
+		total += 4.89 * 25;
+		total += 5.40 * 35;
+		total += 6.41 * 40;
+		total += 7.16 * (userInput - 100);
+	} else if (userInput > 150 && userInput < 200){
+		total += 4.89 * 25;
+		total += 5.40 * 35;
+		total += 6.41 * 40;
+		total += 7.16 * 50;
+		total += 7.33 * (userInput - 150);
+	} else if (userInput > 200 && userInput < 300){
+		total += 4.89 * 25;
+		total += 5.40 * 35;
+		total += 6.41 * 40;
+		total += 7.16 * 50;
+		total += 7.33 * 50;
+		total += 7.33 * (userInput - 200);
+	}  else if (userInput > 300){
+		total += 4.89 * 25;
+		total += 5.40 * 35;
+		total += 6.41 * 40;
+		total += 7.16 * 50;
+		total += 7.33 * 50;
+		total += 7.33 * 100;
+		total += 8.92 * (userInput - 300);
+	}
 	
-	if 
 	
+	printf("Then your total amount would be: %.4f\n", total);
 	return 0;
 }
