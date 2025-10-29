b65cb1f767ae5ed4cc0dd16223c6bfccf9455044
SandeepKumarKuanar <sandeepkumarkuanar3@gmail.com>
2025-10-29 13:18:55 +0530

    to the week-5

--- Week-5/Q4.c
+++ Week-5/Q4.c
@@ -4,44 +4,11 @@
  */
 #include<stdio.h>
 
-int grace_marks(int *a, int *length){
-	for(int i = 0; i < (*length) ; i++){
-		(*(a + (i * sizeof(i))) + 5);		
-	}
-}
-
-
-
-
-
 int main(){
-	int lengthOfArray = 0;
-	int marks[5] = {12, 23, 34, 45, 56};
-	printf("This is the array of the 5 students and their marks\n");
-
-	lengthOfArray = sizeof(marks) / sizeof(marks[0]);
-	printf("[");
-	for(int i = 0; i < lengthOfArray; i++){
-		printf("%d <- %ld ;", marks[i], (*marks)+ (i * (sizeof(marks[i]))));
-	}
-	printf("]\n");
-
-	printf("This is the updated array after the implementation of the grace function\n");
-	printf("[");
-
-	grace_marks(marks, &lengthOfArray);
-
-	for(int i = 0; i < lengthOfArray; i++){
-		printf("%d <- %ld ;", marks[i], (*marks)+ (i * (sizeof(marks[i]))));
-	}
-	printf("]\n");
-
-
+	int a[3] = {5, 4, 3};
+	int *p;
+	printf("check, a = %d\n", *(a + 4));
 	return 0;
 }
-
-
-
-
 
 
